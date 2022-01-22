0a7a2a1faee7f212def642e6df71de60e4fdaccf
shinsw627 <shinsw627@naver.com>
2022-01-22 22:30:19 +0900

    init

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { AppController } from './app.controller';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [],
+})
+export class AppModule implements NestModule {
+  configure() {
+    const DEBUG = process.env.MODE === 'dev' ? true : false;
+    mongoose.set('debug', DEBUG);
+  }
+}
