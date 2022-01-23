f9b715466193ac776794c8c169d9c766bca4fdd0
shinsw627 <shinsw627@naver.com>
2022-01-23 22:08:13 +0900

    Implement chat service

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,4 @@
-const socket = io('/');
+const socket = io('/chattings');
 
 const getElementById = (id) => document.getElementById(id) || null;
 
@@ -6,12 +6,56 @@ const helloStrangerElement = getElementById('hello_stranger');
 const chattingBoxElement = getElementById('chatting_box');
 const formElement = getElementById('chat_form');
 
+// global socket handler
+socket.on('user_connected', (username) => {
+  drawNewChat(`${username}님이 입장하셨습니다.`);
+});
+
+socket.on('user_disconnected', (username) => {
+  drawNewChat(`${username}님이 퇴장하셨습니다.`);
+});
+
+socket.on('new_chat', (data) => {
+  const { chat, username } = data;
+  drawNewChat(`${username} : ${chat}`);
+});
+
+// event callback functions
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const inputValue = event.target.elements[0].value;
+  if (inputValue !== '') {
+    socket.emit('submit_chat', inputValue);
+
+    // 화면에 채팅표시
+    drawNewChat(`나 : ${inputValue}`);
+    event.target.elements[0].value = '';
+  }
+};
+
+// draw functions
+const drawHelloStranger = (username) =>
+  (helloStrangerElement.innerText = `어서오세요 ${username}님! :)`);
+
+const drawNewChat = (message) => {
+  const wrapperChatBox = document.createElement('div');
+  const chatBox = `
+    <div>
+      ${message}
+    </div>
+  `;
+  wrapperChatBox.innerHTML = chatBox;
+  chattingBoxElement.append(wrapperChatBox);
+};
+
 function helloUser() {
   const username = prompt('what is your name?');
-  socket.emit('new_user');
+  socket.emit('new_user', username, (data) => {});
+  drawHelloStranger(username);
 }
 
 function init() {
   helloUser();
+  formElement.addEventListener('submit', handleSubmit);
 }
 init();
--- src/chats.gateway.ts
+++ src/chats.gateway.ts
@@ -1,19 +1,63 @@
+import { Logger } from '@nestjs/common';
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class ChatsGateway {
+@WebSocketGateway({ namespace: 'chattings' })
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger = new Logger('chat');
+
+  afterInit(server: any) {
+    this.logger.log('chat init');
+  }
+
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    this.logger.log(
+      `소켓 연결됨 | socket.id : ${socket.id}, nsp.name : ${socket.nsp.name}`,
+    );
+  }
+
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    this.logger.log(
+      `소켓 연결 해제 | socket.id : ${socket.id}, nsp.name: ${socket.nsp.name}`,
+    );
+  }
+
   @SubscribeMessage('new_user')
   handleNewUser(
     @MessageBody() username: string,
     @ConnectedSocket() socket: Socket,
   ) {
-    console.log(username);
-    console.log(socket.id);
+    socket.broadcast.emit('user_connected', username);
+    return username;
+  }
+
+  @SubscribeMessage('disconnecting')
+  handleLeftUser(
+    @MessageBody() username: string,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    socket.broadcast.emit('user_disconnected', username);
+    return username;
+  }
+
+  @SubscribeMessage('submit_chat')
+  handleSubmitChat(
+    @MessageBody() chat: string,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    socket.broadcast.emit('new_chat', {
+      chat,
+      username: socket.id,
+    });
   }
 }
